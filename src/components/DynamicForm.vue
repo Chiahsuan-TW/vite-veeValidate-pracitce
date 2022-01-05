fb8a041b733b34d0d714cb7aec5ee219ddd8a21c
Chiahsuan Chou <belinda05021992@gmail.com>
2022-01-05 12:20:01 +0800

    add dynamic form

---
+++ src/components/DynamicForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <Form @submit="onSubmit">
+    <div v-for="{ as, name, label, ...attrs } in schema.fields" :key="name">
+      <label :for="name">{{ label }}</label>
+      <Field :id="name" :as="as" :name="name" v-bind="attrs" />
+      <ErrorMessage :name="name" class="error"/>
+    </div>
+    <button>Submit</button>
+  </Form>
+</template>
+
+<script>
+import { Form, Field, ErrorMessage } from "vee-validate";
+
+export default {
+  name: "DynamicForm",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  props: {
+    schema: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    onSubmit(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  max-width: 500px;
+  outline: 1px solid #fd6;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .error {
+    color: rgb(221, 87, 110);
+    font-weight: 700;
+  }
+
+  button {
+    padding: 5px;
+    margin-top: 5px;
+    background-color: #cce;
+    border-radius: 4px;
+  }
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,55 @@
 <template>
   <h1>表單</h1>
   <component :is="currentForm"></component>
-  
+  <DynamicForm :schema="formSchema" />
 </template>
 
 <script>
-import BasicForm from "./../components/BasicForm.vue"
+import BasicForm from "./../components/BasicForm.vue";
+import DynamicForm from "./../components/DynamicForm.vue";
+import * as yup from "yup";
 export default {
   components: {
     BasicForm,
+    DynamicForm,
   },
   data() {
+    const formSchema = {
+      fields: [
+        {
+          label: "Your Name",
+          name: "name",
+          as: "input",
+          rules: yup.string().required(),
+        },
+        {
+          label: "Your Email",
+          name: "email",
+          as: "input",
+          rules: yup.string().email().required(),
+        },
+        {
+          label: "Your Password",
+          name: "password",
+          as: "input",
+          type: "password",
+          rules: yup.string().min(6).required(),
+        },
+        {
+          label: "Comments",
+          name: "comments",
+          as: "textarea",
+          rules: yup.string().max(50).required(),
+        },
+      ],
+    };
     return {
-      currentForm: 'BasicForm',
+      formSchema,
+      currentForm: "BasicForm",
       // form: ['BasicForm',]
-    }
-  }
-
-}
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
