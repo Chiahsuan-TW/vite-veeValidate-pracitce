7ed2e5e6a72e34b62d41b22b6d0296ad47400705
Chiahsuan Chou <belinda05021992@gmail.com>
2022-01-05 10:55:12 +0800

    extract the from to be component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,25 @@
 <template>
-  <h1>Home</h1>
-  <Form @submit="onSubmit">
-    <Field type="email" name="email" :rules="validateEmail"/>
-    <ErrorMessage name="email"/>
-    <button>Sign up for newsletter</button>
-  </Form>
+  <h1>表單</h1>
+  <component :is="currentForm"></component>
+  
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from "vee-validate";
-
+import BasicForm from "./../components/BasicForm.vue"
 export default {
   components: {
-    Form,
-    Field,
-    ErrorMessage,
+    BasicForm,
   },
-  methods: {
-    onSubmit(values) {
-      console.log("Submitted", values);
-    },
-    validateEmail(value) {
-      // if the field is empty
-      if (!value) {
-        return 'This field is required';
-      }
-      // if the field is not a valid email
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!regex.test(value)) {
-        return 'This field must be a valid email';
-      }
-      // All is good
-      return true;
-    },
-  },
-};
+  data() {
+    return {
+      currentForm: 'BasicForm',
+      // form: ['BasicForm',]
+    }
+  }
+
+}
 </script>
 
-<style lang="scss" scoped>
-form {
-  outline: 1px solid rgb(25, 44, 7);
-  max-width: 500px;
-  margin: 0 auto;
+<style>
 
-  input {
-    padding: 5px;
-  }
-
-  button {
-    padding: 5px;
-    background: #efd;
-    box-shadow: 2px 2px 1px #4e4;
-  }
-
-  button:hover {
-    cursor: pointer;
-  }
-}
 </style>
